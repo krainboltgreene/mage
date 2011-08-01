a25103921800dc365204cba8041b8d09d952e083
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-08-01 16:43:25 -0700

    Adding windows inital support and commnads.
    Signed-off-by: Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>

---
+++ lib/mage/windows/software.rb
@@ -0,0 +1,147 @@
+module Mage
+  class Windows
+    def os_name
+
+    end
+
+    def os_version
+
+    end
+
+    def os_manufacturer
+
+    end
+
+    def os_version
+
+    end
+
+    def os_configuration
+
+    end
+
+    def os_build_type
+
+    end
+
+    def registered_owner
+
+    end
+
+    def registered_organization
+
+    end
+
+    def product_id
+
+    end
+
+    def original_install_date
+
+    end
+
+    def system_boot_time
+
+    end
+
+    def system_manufacturer
+
+    end
+
+    def system_model
+
+    end
+
+    def system_type
+
+    end
+
+    def processors
+
+    end
+
+    def bios_version
+
+    end
+
+    def windows_directory
+
+    end
+
+    def system_directory
+
+    end
+
+    def boot_device
+
+    end
+
+    def system_locale
+
+    end
+
+    def system_locale
+
+    end
+
+    def input_locale
+
+    end
+
+    def time_zone
+
+    end
+
+    def total_physical_memory
+
+    end
+
+    def available_physical_memory
+
+    end
+
+    def page_max_size
+
+    end
+
+    def page_available
+
+    end
+
+    def page_in_use
+
+    end
+
+    def page_location
+
+    end
+
+    def domain
+
+    end
+
+    def logon_server
+
+    end
+  end
+end
+
+
+
+# Processor(s):              1 Processor(s) Installed.
+#                            [01]: x64 Family 16 Model 2 Stepping 2 AuthenticAMD ~2200 Mhz
+# BIOS Version:              Phoenix Technologies, LTD  5.04, 12/12/2007
+# Windows Directory:         C:\Windows
+# System Directory:          C:\Windows\system32
+# Boot Device:               \Device\HarddiskVolume1
+# System Locale:             en-us;English (United States)
+# Input Locale:              en-us;English (United States)
+# Time Zone:                 (GMT-05:00) Eastern Time (US & Canada)
+# Total Physical Memory:     3,070 MB
+# Available Physical Memory: 855 MB
+# Page File: Max Size:       6,354 MB
+# Page File: Available:      2,536 MB
+# Page File: In Use:         3,818 MB
+# Page File Location(s):     C:\pagefile.sys
+# Domain:                    WORKGROUP
+# Logon Server:              \\AYBABTU
