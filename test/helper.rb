66cdff9483f365c67d041f5024b1c56100e8b2b6
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2011-10-24 23:05:02 -0500

    Adding a scaffold test directory.

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,8 @@
-# require 'bundler/setup'
-# require 'minitest/autorun'
+require 'bundler/setup'
+require 'minitest/autorun'
+SimpleCov.start
 
-# unless Object.const_defined? 'Mage'
-#   $:.unshift File.expand_path '../../lib', __FILE__
-#   require 'mage'
-# end
+unless Object.const_defined? 'Mage'
+  $:.unshift File.expand_path '../../lib', __FILE__
+  require 'mage'
+end
