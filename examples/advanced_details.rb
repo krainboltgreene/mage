f0e7896a1ea53576f9c07897193ebc1ed30cfc9c
Fernando Ortiz <eratos2000@gmail.com>
2011-11-20 21:54:04 -0600

    changed examples

--- examples/advanced_details.rb
+++ examples/advanced_details.rb
@@ -1,12 +1,5 @@
 require 'mage'
 
-my_computer = Mage.build
-
-case my_computer.platform
-  when "Darwin"
-    p my_computer.machine.time_since_boot.class     #=> Fixnum
-    p my_computer.machine.processor_speed           #=> "2.4 GHz"
-  when "Windows"
-    p my_computer.machine.windows_directory         #=> "C:\Windows"
-    p my_computer.machine.product_id                #=> "89578-OEM-7332157-00061"
-end
+my_computer = Mage.build 
+p my_computer.time_since_boot.class     #=> Fixnum
+p my_computer.processor_speed           #=> "2.4 GHz"
