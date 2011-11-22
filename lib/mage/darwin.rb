3447cf958af5caa01d8fa7212214b309a76b5248
Fernando Ortiz <eratos2000@gmail.com>
2011-11-21 21:42:29 -0600

    darwin experimental changes

--- lib/mage/darwin.rb
+++ lib/mage/darwin.rb
@@ -1,20 +1,24 @@
+require_relative 'darwin/hardware'
+require_relative 'darwin/software'
+
 class Mage::Darwin
   include Mage::Interface
 
   attr_accessor :data
 
   def initialize
-    @data = YAML.load prepared `system_profiler SPSoftwareDataType SPNetworkDataType`
+    @data = YAML.load prepared `system_profiler SPSoftwareDataType SPHardwareDataType`
+    @methods = []
+    generate_software
+    generate_hardware
   end
 
   def prepared(profile)
     profile.chomp!
     profile.gsub!(/\[\d+\]:/,'-')
+    profile
   end
 end
-
-require_relative 'darwin/hardware'
-require_relative 'darwin/software'
 
 # SPNetworkDataType
 # SPSoftwareDataType
